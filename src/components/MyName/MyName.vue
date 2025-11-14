cc9b70147c467d59485a6f3e2da26b369be3b906
Jose Dongil <joseantonio.dongil.tech@bbva.com>
2025-11-14 16:36:29 +0100

    FEAT: Nuevo component MyName

--- src/components/MyName/MyName.vue
+++ src/components/MyName/MyName.vue
@@ -16,57 +16,57 @@
 }
 
 .my-names {
-    color: $color-1;
-    font-family: $font-1;
-    font-weight: $weight-font-bold;
-    font-size: 70px;
+  color: $color-1;
+  font-family: $font-1;
+  font-weight: $weight-font-bold;
+  font-size: 70px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  span {
     display: flex;
-    flex-direction: column;
+    width: 100%;
     justify-content: center;
     align-items: center;
+  }
 
-    span {
-      display: flex;
-      width: 100%;
-      justify-content: center;
-      align-items: center;
+  .circle {
+    position: relative;
+    font-family: $font-1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: $color-2;
+    font-weight: $weight-font-regular;
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    font-size: 40px;
+    color: #fff;
+
+    &::before {
+      content: '';
+      left: 65px;
+      background: $color-2;
+      display: inline-block;
+      position: absolute;
+      top: 30px;
+      width: 80px;
+      height: 1px;
     }
 
-    .circle {
-      position: relative;
-      font-family: $font-1;
-      display: flex;
-      justify-content: center;
-      align-items: center;
+    &::after {
+      content: '';
+      right: 65px;
       background: $color-2;
-      font-weight: $weight-font-regular;
-      border-radius: 50%;
-      width: 60px;
-      height: 60px;
-      font-size: 40px;
-      color: #fff;
-
-      &::before {
-        content: '';
-        left: 65px;
-        background: $color-2;
-        display: inline-block;
-        position: absolute;
-        top: 30px;
-        width: 80px;
-        height: 1px;
-      }
-
-      &::after {
-        content: '';
-        right: 65px;
-        background: $color-2;
-        display: inline-block;
-        position: absolute;
-        top: 30px;
-        width: 80px;
-        height: 1px;
-      }
+      display: inline-block;
+      position: absolute;
+      top: 30px;
+      width: 80px;
+      height: 1px;
     }
   }
+}
 </style>
